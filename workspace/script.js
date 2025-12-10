fd007e201ce38f89804fbe3cdaf8c0cafc66d553
Celaerius <131456370+Celaerius@users.noreply.github.com>
2025-12-10 15:55:55 +0100

    zsdv

--- workspace/script.js
+++ workspace/script.js
@@ -1,38 +1,34 @@
-let db = connect("mongodb://root:test123@localhost");
-// équivalent du "use technocite"
-db = db.getSiblingDB("sample_mflix");
+let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
+// USE technocite
+db = db.getSiblingDB('sample_mflix');
 
-db.movies.aggregate([
-    {
-        $match: {
-            'imdb.rating': {
-                $lt: 5
-            }
-        }
-    },
-    {
-        $unwind: '$directors'
-    },
-    {
-        $group: {
-            _id: '$directors',
-            total: {
-                $count: {}
-            }
-        }
-    },
-    {
-        $sort: {
-            total: -1
-        }
-    },
-    {
-        $limit: 10
-    },
-    {
-        $out : {
-            db: 'sample_mflix',
-            coll: 'lame_directors'
-        }
-    }
-]);
+const movies = db.movies
+.find({
+    //cast: "Keanu Reeves",
+    //genres: "Comedy",
+    //year: { $gte: 2002, $lte: 2008 },
+    //$and: [
+    //    { cast: { $eq: "Chris O'Donnell" } },
+    //    { cast: { $eq: "Matt Damon" } }
+    //],
+    //$or: [
+    //{directors: { $eq: "Neil Burger" }},
+    //{directors: { $eq: "Tony Scott" }}
+    //],
+
+    //"imdb.rating": { $gte: 8.0 }
+    //"tomatoes.critic.rating": { $gte: 8 },
+
+    "realeased.date": { $exists: false }
+
+})
+
+console.log(movies);
+
+const latestmovies = db.movies
+.find({})
+.sort({ released: 1 })
+.limit(1)
+
+//console.log(latestmovies);
+
